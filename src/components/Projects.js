1daa41464698717282bc06034abfeea6d5e7cdaf
Daniel Rice <danielrice5627@gmail.com>
2024-03-12 15:39:04 -0700

    finished links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,13 +16,13 @@ function Projects ({projects}) {
                     <div className="project-links">
                         <a
                             rel="apple-touch-icon"
-                            href="https://github.com/danielerice"
+                            href="https://github.com/danielerice/GPTQuest"
                             target="_blank"
                             type="image/png" 
                         >
                             <img src={GitHubLogo} className="project-icon"/>
                         </a>
-                        <a href="#" className="link">
+                        <a href="https://www.loom.com/share/af0489adeb784b2d908fcfdb845306e2?sid=ca14765e-922b-466a-8fc5-1b2e0558c9fe" className="link">
                             Demo
                         </a>
                     </div>
@@ -30,22 +30,20 @@ function Projects ({projects}) {
             </div>
 
             <div className="project-card">
-                <a href="#">
-                    <img src={Codex} alt="" />
-                </a>
+                <img src={Codex} alt="" />
                 <div >
                     <h5 >Recipe Codex</h5>
                     <p >This site utilizes React Ruby and Bootstrap to efficiently organize recipes</p>
                     <div className="project-links">
                         <a
                             rel="apple-touch-icon"
-                            href="https://github.com/danielerice"
+                            href="https://github.com/danielerice/Recipe-Codex"
                             target="_blank"
                             type="image/png" 
                         >
                             <img src={GitHubLogo} className="project-icon"/>
                         </a>
-                        <a href="#" className="link">
+                        <a href="https://www.loom.com/share/64f2617a835546b0972b6fdf990efa5d?sid=9a7eeb0e-65d0-4eef-ad2c-5db75463a0af" className="link">
                             Demo
                         </a>
                     </div>
@@ -53,14 +51,12 @@ function Projects ({projects}) {
             </div>
 
             <div className="project-card">
-                <a href="#">
-                    <img className="" src={Medium} alt="" />
-                </a>
-                <div class="">
+                <img src={Medium} alt="" />
+                <div>
                     <h5 >My Personal Blog</h5>
                     <p >Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     <div className="project-links">
-                        <a href="#" className="link">
+                        <a href="https://medium.com/@danielrice5627" className="link">
                             Link
                         </a>
                     </div>
